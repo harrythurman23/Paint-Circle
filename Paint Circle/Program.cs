3aab0ff4fd8193c1576d47c1ad8870f6661312f9
harrythurman23 <125393979+harrythurman23@users.noreply.github.com>
2023-04-25 18:12:48 -0400

    fixed capitalization
    program now runs

--- Paint Circle/Program.cs
+++ Paint Circle/Program.cs
@@ -6,22 +6,22 @@ namespace Paint_Circle
     {
         static void Main(string[] args)
         {
-            Console.Writeline("what is the radius of the center cricle you need to paint, in feet?");
-            int radius = convert.toint32(console.readline());
-            int area = convert.toint32(radius * radius * math.pi); // can use (int) instead of convert.toint32 for doubles like here; doesnt work on strings
+            Console.WriteLine("what is the radius of the center cricle you need to paint, in feet?");
+            int radius = Convert.ToInt32(Console.ReadLine());
+            int area = Convert.ToInt32(radius * radius * Math.PI); // can use (int) instead of convert.toint32 for doubles like here; doesnt work on strings
 
 
-            console.writeline("what color paint would you like to use? you choices are red, blue, green, and yellow");
-            string color = console.readline().tolower();
+            Console.WriteLine("what color paint would you like to use? you choices are red, blue, green, and yellow");
+            string color = Console.ReadLine().ToLower();
             //use console.tolower(string) too
             if (color == "red")
-            { console.writeline("it will take " + (area / 100 + 1) + " buckets to paint the " + (radius) + "' radius circle red"); }
+            { Console.WriteLine("it will take " + (area / 100 + 1) + " buckets to paint the " + (radius) + "' radius circle red"); }
             if (color == "blue")
-            { console.writeline("it will take " + (area / 120 + 1) + " buckets to paint the " + (radius) + "' radius circle blue"); }
+            { Console.WriteLine("it will take " + (area / 120 + 1) + " buckets to paint the " + (radius) + "' radius circle blue"); }
             if (color == "green")
-            { console.writeline("it will take " + (area / 90 + 1) + " buckets to paint the " + (radius) + "' radius circle green"); }
+            { Console.WriteLine("it will take " + (area / 90 + 1) + " buckets to paint the " + (radius) + "' radius circle green"); }
             if (color == "yellow")
-            { console.writeline("it will take " + (area / 70 + 1) + " buckets to paint the " + (radius) + "' radius circle yellow"); }
+            { Console.WriteLine("it will take " + (area / 70 + 1) + " buckets to paint the " + (radius) + "' radius circle yellow"); }
         }
     }
 }
